e6ba59f5fb43597ee97e08b75c1f97e8c0886401
Juergen Belz <juergen.muenzner@web.de>
2018-03-06 23:29:42 +0100

    added the complete testing environment

--- INA219_brzo_simple/INA219_brzo_simple.ino
+++ INA219_brzo_simple/INA219_brzo_simple.ino
@@ -1,7 +1,6 @@
 #include <brzo_i2c.h>
 #include <INA219_brzo.h>
 INA219_brzo ina;
-// scl speed is currently hardcoded
 
 void setup() {
   unsigned long BAUDRATE = 500000;
@@ -10,7 +9,7 @@ void setup() {
   Serial.println("-----------------------------------------------");
   ina.begin(0x40);
   ina.configure(INA219_RANGE_16V, INA219_GAIN_40MV, INA219_BUS_RES_9BIT, INA219_SHUNT_RES_9BIT_1S, INA219_MODE_SHUNT_CONT);
-  ina.calibrate(0.1, 0.05); // 0.1ohm, 50mA
+  ina.calibrate(0.1, 0.1); // 0.1ohm, 50mA
   delay(2000);
 }
 
@@ -21,30 +20,108 @@ unsigned long t1;
 unsigned long t2;
 
 #define READ_LIMIT 1000
-void loop() {
-  // 550µs seem to be the limit , its actually limited by the serial bus as it seems ( ~600µs)
-  // This means i should add a buffer with about 10 entries before sending the serial out
+uint8_t mybuffer[3];
 
+void loop() {
   cnt++;
   if (cnt == 1) {
     t1 = micros();
   }
   ina_current = ina.readShuntCurrent();
-  //ina_bus = ina.readBusVoltage();
+
   if (cnt == READ_LIMIT) {
+    checkConfig();
     t2 = micros();
 
     Serial.print((t2 - t1));
     Serial.print(" µs; ");
     Serial.print((float)(t2 - t1) / READ_LIMIT, 5);
     Serial.print(" µs/READ; ");
-    Serial.print(ina_current * 1000.0, 1);
+    Serial.print(ina_current, HEX);
     Serial.print(" mA;");
-    Serial.print(ina_bus, 3);
+    Serial.print(ina_bus, 10);
     Serial.print(" V");
     Serial.println("");
     Serial.println("");
     cnt = 0;
   }
 
+}
+
+void checkConfig()
+{
+  Serial.print("Mode:                 ");
+  switch (ina.getMode())
+  {
+    case INA219_MODE_POWER_DOWN:      Serial.println("Power-Down"); break;
+    case INA219_MODE_SHUNT_TRIG:      Serial.println("Shunt Voltage, Triggered"); break;
+    case INA219_MODE_BUS_TRIG:        Serial.println("Bus Voltage, Triggered"); break;
+    case INA219_MODE_SHUNT_BUS_TRIG:  Serial.println("Shunt and Bus, Triggered"); break;
+    case INA219_MODE_ADC_OFF:         Serial.println("ADC Off"); break;
+    case INA219_MODE_SHUNT_CONT:      Serial.println("Shunt Voltage, Continuous"); break;
+    case INA219_MODE_BUS_CONT:        Serial.println("Bus Voltage, Continuous"); break;
+    case INA219_MODE_SHUNT_BUS_CONT:  Serial.println("Shunt and Bus, Continuous"); break;
+    default: Serial.println("unknown");
+  }
+  
+  Serial.print("Range:                ");
+  switch (ina.getRange())
+  {
+    case INA219_RANGE_16V:            Serial.println("16V"); break;
+    case INA219_RANGE_32V:            Serial.println("32V"); break;
+    default: Serial.println("unknown");
+  }
+
+  Serial.print("Gain:                 ");
+  switch (ina.getGain())
+  {
+    case INA219_GAIN_40MV:            Serial.println("+/- 40mV"); break;
+    case INA219_GAIN_80MV:            Serial.println("+/- 80mV"); break;
+    case INA219_GAIN_160MV:           Serial.println("+/- 160mV"); break;
+    case INA219_GAIN_320MV:           Serial.println("+/- 320mV"); break;
+    default: Serial.println("unknown");
+  }
+
+  Serial.print("Bus resolution:       ");
+  switch (ina.getBusRes())
+  {
+    case INA219_BUS_RES_9BIT:         Serial.println("9-bit"); break;
+    case INA219_BUS_RES_10BIT:        Serial.println("10-bit"); break;
+    case INA219_BUS_RES_11BIT:        Serial.println("11-bit"); break;
+    case INA219_BUS_RES_12BIT:        Serial.println("12-bit"); break;
+    default: Serial.println("unknown");
+  }
+
+  Serial.print("Shunt resolution:     ");
+  switch (ina.getShuntRes())
+  {
+    case INA219_SHUNT_RES_9BIT_1S:    Serial.println("9-bit / 1 sample"); break;
+    case INA219_SHUNT_RES_10BIT_1S:   Serial.println("10-bit / 1 sample"); break;
+    case INA219_SHUNT_RES_11BIT_1S:   Serial.println("11-bit / 1 sample"); break;
+    case INA219_SHUNT_RES_12BIT_1S:   Serial.println("12-bit / 1 sample"); break;
+    case INA219_SHUNT_RES_12BIT_2S:   Serial.println("12-bit / 2 samples"); break;
+    case INA219_SHUNT_RES_12BIT_4S:   Serial.println("12-bit / 4 samples"); break;
+    case INA219_SHUNT_RES_12BIT_8S:   Serial.println("12-bit / 8 samples"); break;
+    case INA219_SHUNT_RES_12BIT_16S:  Serial.println("12-bit / 16 samples"); break;
+    case INA219_SHUNT_RES_12BIT_32S:  Serial.println("12-bit / 32 samples"); break;
+    case INA219_SHUNT_RES_12BIT_64S:  Serial.println("12-bit / 64 samples"); break;
+    case INA219_SHUNT_RES_12BIT_128S: Serial.println("12-bit / 128 samples"); break;
+    default: Serial.println("unknown");
+  }
+
+  Serial.print("Max possible current: ");
+  Serial.print(ina.getMaxPossibleCurrent());
+  Serial.println(" A");
+
+  Serial.print("Max current:          ");
+  Serial.print(ina.getMaxCurrent());
+  Serial.println(" A");
+
+  Serial.print("Max shunt voltage:    ");
+  Serial.print(ina.getMaxShuntVoltage());
+  Serial.println(" V");
+
+  Serial.print("Max power:            ");
+  Serial.print(ina.getMaxPower());
+  Serial.println(" W");
 }
